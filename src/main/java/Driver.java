6c737ee04198fbf3ad7b4e91fe3478ffda6c06f7
grevillopes <grevillopes@gmail.com>
2017-06-09 21:19:11 -0500

    initial commit

---
+++ src/main/java/Driver.java
@@ -0,0 +1,8 @@
+public class Driver {
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Testing gradle");
+    }
+}
