278f7b328c9e058d2af62efa7b77b4353fa8809a
grevillopes <grevillopes@gmail.com>
2017-06-09 22:12:06 -0500

    created and filled deck of cards

--- src/main/java/Driver.java
+++ src/main/java/Driver.java
@@ -1,8 +1,35 @@
+
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Deque;
+import java.util.List;
+
 public class Driver {
+
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Testing gradle");
+        String[] suites = {"hearts", "spades", "clubs", "diamonds"};
+        List<String> deck = new ArrayList<String>();
+        
+        for(String suit: suites){
+            deck.add(suit + "-Ace"); 
+            for(int i=2; i<=10; i++){
+                deck.add(suit + "-" + i);
+            }
+            deck.add(suit + "-Jack");
+            deck.add(suit + "-Queen");
+            deck.add(suit + "-King");
+            
+            
+            
+        }
+        System.out.println(deck);
+
+
     }
+
 }
+
